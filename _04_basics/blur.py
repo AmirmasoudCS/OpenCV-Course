16c1038bed730ff15ed605b8336ff03b377e7b41
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 18:22:29 +0330

    Adding the comment to how blur more.

--- _04_basics/blur.py
+++ _04_basics/blur.py
@@ -3,5 +3,6 @@ import cv2 as cv
 def blur(image):
 
     blured_image = cv.GaussianBlur(image, (3,3), cv.BORDER_DEFAULT) # ksize is the kernel size that we are going to apply the Gaussian blur on which needs to be an odd by odd set of numbers: (odd, odd)
+                                                                    # If you want to blur the image further more, you have to increase the kernel size of it.
 
     return blured_image
