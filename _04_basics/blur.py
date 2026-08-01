598c46e26f6c24e9ba72de76f0afa2126ff3c874
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 18:21:29 +0330

    Removing the explicit passing of arguments so we can run the code.

--- _04_basics/blur.py
+++ _04_basics/blur.py
@@ -2,6 +2,6 @@ import cv2 as cv
 
 def blur(image):
 
-    blured_image = cv.GaussianBlur(src=image, ksize=(3,3), borderType=cv.BORDER_DEFAULT) # ksize is the kernel size that we are going to apply the Gaussian blur on which needs to be an odd by odd set of numbers: (odd, odd)
+    blured_image = cv.GaussianBlur(image, (3,3), cv.BORDER_DEFAULT) # ksize is the kernel size that we are going to apply the Gaussian blur on which needs to be an odd by odd set of numbers: (odd, odd)
 
     return blured_image
