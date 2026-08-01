f2313d50c8e8cdef8dc71b265400f88effbc6647
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 11:33:51 +0330

    Implementing the code for the reading and rescaling vidoe frames side by side.

---
+++ rescale/rescale_video.py
@@ -0,0 +1,33 @@
+# Displaynig large scaled vidoes and images, which contain a lot of details, can be very slow.
+# By rescaling them, we can speed up the display and reduce memory usage, while still preserving the overall appearance of the content.
+
+import cv2 as cv
+
+def rescaleFrame(frame, scale=0.75): # Function to rescale the frame by a given scale factor
+
+    width = frame.shape[1] * scale # frame.shape[1] gives the width of the frame, and we multiply it by the scale factor to get the new width
+    height = frame.shape[0] * scale # frame.shape[0] gives the height of the frame, and we multiply it by the scale factor to get the new height
+
+    width = int(width) # Converting the width to an integer
+    height = int(height) # Converting the height to an integer
+
+    dimensions = (width, height) # Creating a tuple of the new dimensions
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA) # Resizing the frame using the new dimensions and returning it
+
+capture = cv.VideoCapture("Videos/vid1.mp4") # Creating a VideoCapture object to read the video file
+
+while True: # Infinite loop to read and display frames from the video
+
+    isTrue, frame = capture.read() # Reading a frame from the video
+
+    resized_frame = rescaleFrame(frame, scale=0.5) # Rescaling the frame by a scale factor of 0.5
+
+    cv.imshow("Video", frame) # Displaying the original frame
+    cv.imshow("Resized Video", resized_frame) # Displaying the rescaled frame
+
+    if cv.waitKey(20) & 0xFF == ord('d'): # Breaking the loop if 'd' is pressed
+        break
+
+capture.release() # Releasing the VideoCapture object
+cv.destroyAllWindows() # Closing all OpenCV windows
