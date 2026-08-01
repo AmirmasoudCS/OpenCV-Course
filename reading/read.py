7b0861236543d753e467a058b29ec1618ab79e1b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 10:50:35 +0330

    Reading and displaying an image using imread and imshow methods in the opencv library.

--- reading/read.py
+++ reading/read.py
@@ -1,1 +1,7 @@
 import cv2 as cv
+
+img = cv.imread("Photos/img1.jpg") # Reading an image
+
+cv.imshow("Image", img) # Showing the image variable in a window called "Image"
+
+cv.waitkey(0) # Waiting for infinite amount of time for a key press to close the window
