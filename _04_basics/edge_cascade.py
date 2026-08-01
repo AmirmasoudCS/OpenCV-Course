3e9ab8e0791d41998c06a411f634abf6a0a9107e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 18:27:19 +0330

    Refactoring the function to take in different values of thresholds.

--- _04_basics/edge_cascade.py
+++ _04_basics/edge_cascade.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 
-def find_edge(image):
+def find_edge(image, th1, th2):
 
-    canny = cv.Canny(image, 125, 175) # 125 and 175 are respectively threshold1 and threshold2 values
+    canny = cv.Canny(image, th1, th2) # 125 and 175 are respectively threshold1 and threshold2 values
 
     return canny
