37ef2507e6dcaa28c2986812ab59595eaf326384
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 10:57:45 +0330

    Adding a comment.

--- reading/read.py
+++ reading/read.py
@@ -6,7 +6,7 @@ cv.imshow("Image", img) # Showing the image variable in a window called "Image"
 
 cv.waitKey(delay=0) # Waiting for infinite amount of time for a key press to close the window
 
-large_img = cv.imread("Photos/img7_large.jpg") # Reading a large image
+large_img = cv.imread("Photos/img7_large.jpg") # Reading a large image, readung a large image might go out of bounds of screen size, so resizing it to fit the screen size might be needed
 
 cv.imshow("Large Image", large_img) # Showing the large image variable in a window called "Large Image"
 
