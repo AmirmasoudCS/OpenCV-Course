249cebf2b8367f204ef7b0b33f1869f941433193
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 18:08:17 +0330

    Adding the menu option to convert an image to grey.

---
+++ _04_basics/basic.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+from _04_basics.grey_scale import to_grey
+
+def main():
+
+    image = cv.imread("assets/Photos/image4.jpg")
+
+    choice = "What do  you want to do?\n(1) Convert to grey scale\n"
+
+    if choice == "1":
+        
+        grey_image = to_grey(image)
+
+        cv.imshow("Grey Image", grey_image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
