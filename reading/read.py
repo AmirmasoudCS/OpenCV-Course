b9ba401a9a417060ef647278db18adb3d6de369c
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 10:57:01 +0330

    Reading a large image might get out of window bounds and migth need resizing.

--- reading/read.py
+++ reading/read.py
@@ -5,3 +5,9 @@ img = cv.imread("Photos/img1.jpg") # Reading an image
 cv.imshow("Image", img) # Showing the image variable in a window called "Image"
 
 cv.waitKey(delay=0) # Waiting for infinite amount of time for a key press to close the window
+
+large_img = cv.imread("Photos/img7_large.jpg") # Reading a large image
+
+cv.imshow("Large Image", large_img) # Showing the large image variable in a window called "Large Image"
+
+cv.waitKey(delay=0) # Waiting for infinite amount of time for a key press to close the window
