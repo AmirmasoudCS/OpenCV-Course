892ff455ce4617f2d4cb2b4134d719506935f838
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 12:17:09 +0330

    Adding a function to paint an image.

--- draw/draw.py
+++ draw/draw.py
@@ -1,8 +1,30 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((500, 500), dtype='uint8')
+# 1. Paint the image a certain color
+def paint_image(image):
 
-cv.imshow("Blank Image", blank)
+    choice = input("waht color do you want to paint the image? (red, green, blue):\n ")
 
-cv.waitKey(0)
+    if choice == "red":
+        image[:] = [0, 0, 255]  # BGR format for red
+    elif choice == "green":
+        image[:] = [0, 255, 0]  # BGR format for green
+    elif choice == "blue":
+        image[:] = [255, 0, 0]  # BGR format for blue
+    else:
+        print("Invalid color choice. Please choose red, green, or blue.")
+
+    return image
+
+def main():
+
+    blank = np.zeros((500, 500), dtype='uint8')
+
+    choice = input("What do you want to do with the image?\n (1) Paint the image.")
+
+    if choice == "1":
+        painted_image = paint_image(blank)
+        cv.imshow("Painted Image", painted_image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
