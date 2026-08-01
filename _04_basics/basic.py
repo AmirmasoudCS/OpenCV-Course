9e091c0ef2cc24bf8f8b0f852116f0adcf442b90
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 18:20:03 +0330

    Adding the menu option for blurring an image.

--- _04_basics/basic.py
+++ _04_basics/basic.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 from _04_basics.grey_scale import to_grey
+from _04_basics.blur import blur
 
 def main():
 
     image = cv.imread("assets/Photos/img4.jpg")
 
-    choice = input("What do  you want to do?\n(1) Convert to grey scale\n")
+    choice = input("What do  you want to do?\n(1) Convert to grey scale\n(2) Blur the image.\n")
 
     if choice == "1":
         
@@ -15,6 +16,20 @@ def main():
         cv.imshow("Grey Image", grey_image)
         cv.waitKey(0)
         cv.destroyAllWindows()
+
+    elif choice == "2":
+
+        blured_image = blur(image)
+
+        cv.imshow("Original Image", image)
+        cv.imshow("Blurred Image", blured_image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    else:
+        print("Wrong input!")
+        return
+    
 
 if __name__ == "__main__":
     main()
