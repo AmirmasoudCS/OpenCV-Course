09e1c79e6373e442a62ab456720c5183398844fb
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 18:43:53 +0330

    Adding default values to the function

--- _04_basics/edge_cascade.py
+++ _04_basics/edge_cascade.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-def find_edge(image, th1, th2):
+def find_edge(image, th1=200, th2=200):
 
     canny = cv.Canny(image, th1, th2) # 125 and 175 are respectively threshold1 and threshold2 values
 
