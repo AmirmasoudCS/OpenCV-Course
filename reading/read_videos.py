1ba279eb37d50198a545c9108469aeba6a3f6bac
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 11:03:26 +0330

    Implementing the code for reading and displaying videos using opencv library frame by frame.

---
+++ reading/read_videos.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+capture = cv.VideoCapture("Videos/vid1.mp4") # Reading a video file from a path given to it
+
+# You can also use a webcam by passing 0 or 1 to the VideoCapture function: cv.VideoCapture(0) or cv.VideoCapture(1)
+
+while True:
+
+    isTrue, frame = capture.read() # Reading the video frame by frame
+
+    cv.imshow("Video", frame) # Displaying the video frame by frame
+
+    if cv.waitKey(20) & 0xFF==ord('d'): # Press 'd' to exit the video
+        break
+
+capture.release() # Releasing the video capture object
+cv.destroyAllWindows() # Destroying all the windows opened by OpenCV
