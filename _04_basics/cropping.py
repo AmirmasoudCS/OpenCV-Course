e17be6e79bd2755bdaaae29e997a1608d2dc4d6a
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 19:17:41 +0330

    Ensuring the type of the slicing in the cropping function.

--- _04_basics/cropping.py
+++ _04_basics/cropping.py
@@ -1,5 +1,8 @@
 import cv2 as cv
 
 def crop(image, start_y, finish_y, start_x, finish_x):
-
-    return image[start_y:finish_y, start_x:finish_x]
+    # Ensure all coordinates are integers
+    sy, fy = int(start_y), int(finish_y)
+    sx, fx = int(start_x), int(finish_x)
+    
+    return image[sy:fy, sx:fx]
