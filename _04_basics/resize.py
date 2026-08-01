6af33e65a6606c8c3ce966775a09f51a3c0f1abb
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-01 19:08:35 +0330

    Adding comments for the interpolation.

--- _04_basics/resize.py
+++ _04_basics/resize.py
@@ -2,6 +2,8 @@ import cv2 as cv
 
 def resize(image, new_size: tuple[int, int] = (500, 500)):
 
-    resized_image = cv.resize(image, new_size)
-
-    return resized_image
+    resized_image = cv.resize(image, new_size)  # There are different interpolation options:
+                                                # (1) cv.INTER_AREA --> Shrinking the images
+                                                # (2) cv.INTER_LINEAR --> Enlarging the images
+                                                # (3) cv.INTER_CUBIC --> Slowest of them all but better results in the end
+    return resized_image    
