d7073628f10498cb527c319528f2e9391137b796
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 22:22:06 +0330

    Fixing a missplacement of words.

--- _08_color_channels/main.py
+++ _08_color_channels/main.py
@@ -11,8 +11,8 @@ def main():
     b, g, r = cv.split(image)
 
     cv.imshow("Blue Channels", b)   # These images are shown in greyscael, and the meaning of pixels in greyscale
-    cv.imshow("Green Channels", g)  # is that wherever it is darker, the density of that color in that specific
-    cv.imshow("Red Channels", r)    # area is higher, and when it is lighter, the absence of that color.
+    cv.imshow("Green Channels", g)  # is that wherever it is lighter, the density of that color in that specific
+    cv.imshow("Red Channels", r)    # area is higher, and when it is darker, the absence of that color.
 
     print(image.shape)
     print(b.shape)
