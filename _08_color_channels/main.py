3c522f226731520af1da327ffd1ab13b34136d7b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 22:24:25 +0330

    Adding a comment on what the shapes numbers means.

--- _08_color_channels/main.py
+++ _08_color_channels/main.py
@@ -12,10 +12,10 @@ def main():
 
     cv.imshow("Blue Channels", b)   # These images are shown in greyscael, and the meaning of pixels in greyscale
     cv.imshow("Green Channels", g)  # is that wherever it is lighter, the density of that color in that specific
-    cv.imshow("Red Channels", r)    # area is higher, and when it is darker, the absence of that color.
+    cv.imshow("Red Channels", r)    # area is higher, and when it is darker, the absence of that color
 
-    print(image.shape)
-    print(b.shape)
+    print(image.shape) # The third argument that gets printed, is the number of its colored channels
+    print(b.shape)     # There is no third argument in the shape of the blue, green, and red colors since they only represent one color
     print(g.shape)
     print(r.shape)
         
