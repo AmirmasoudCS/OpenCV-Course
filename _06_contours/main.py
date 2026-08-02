85755475d36347875d0a65635ab4667ba05cf002
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 12:13:21 +0330

    Blurring the image before finding any edges.

--- _06_contours/main.py
+++ _06_contours/main.py
@@ -3,13 +3,15 @@
 import cv2 as cv
 from _04_basics.grey_scale import to_grey
 from _04_basics.edge_cascade import find_edge
+from _04_basics.blur import blur
 from _06_contours.find_contours import find_contours
 
 def main():
      
     img = cv.imread("assets/Photos/eiffel_tower_2.jpg")
     grey_img = to_grey(img)
-    canny_image = find_edge(grey_img, th1=125, th2=175)
+    blurred_image = blur(grey_img)
+    canny_image = find_edge(blurred_image, th1=125, th2=175)
     contours, hierarchies = find_contours(canny_image)
 
     print(f"{len(contours)} contour(s) were found!")
