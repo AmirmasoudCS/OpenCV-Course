e4a5694b65b574b31a668ec3b560bd0ef63fd3ee
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 12:21:59 +0330

    Adding a comment on what does thresholding means.

--- _06_contours/threshold.py
+++ _06_contours/threshold.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-def threshold(img, threshold=125, maximum=255):
+def threshold(img, threshold=125, maximum=255): # Thresholding basically tries to look at an image and binarise the image.
 
     ret, thresh = cv.threshold(img, threshold, maximum)
 
