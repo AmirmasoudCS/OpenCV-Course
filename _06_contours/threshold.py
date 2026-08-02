ad14c47d2647a8a817c44b0c8eff86ffa4652b62
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 12:23:31 +0330

    Adding another comment to threshold to describe what threshold and maximum value means.

--- _06_contours/threshold.py
+++ _06_contours/threshold.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
 def threshold(img, threshold=125, maximum=255): # Thresholding basically tries to look at an image and binarise the image.
+                                                # with threshold=125 and maximum=255 --> pixels that are <125 := 0 and pixels that are >125 := 255
 
     ret, thresh = cv.threshold(img, threshold, maximum)
 
