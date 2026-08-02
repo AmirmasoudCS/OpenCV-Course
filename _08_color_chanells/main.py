4690034b2ef764368de61f17a26aa5c1beb992ba
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 22:15:38 +0330

    Implementing the code to split an image into its base color channels.

---
+++ _08_color_chanells/main.py
@@ -0,0 +1,19 @@
+# All the colored images, consist of different color channels, Red, Green, Blue
+# Using OpenCV, you can take an image and split it into its color channels
+
+import cv2 as cv
+import numpy as np
+
+def main():
+
+    image = cv.imread("assets/Photos/img7_large.jpg")
+
+    b, g, r = cv.split(image)
+
+    cv.imshow("Blue Channels", b)
+    cv.imshow("Green Channels", g)
+    cv.imshow("Red Channels", r)
+
+
+if __name__ == "__main__":
+    main()
