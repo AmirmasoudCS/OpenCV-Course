8f46b00a72b32448b361849eaac5f434f5f8c7c2
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 12:32:22 +0330

    Passing the type of threshold as binary.

--- _06_contours/threshold.py
+++ _06_contours/threshold.py
@@ -3,6 +3,6 @@ import cv2 as cv
 def threshold(img, threshold=125, maximum=255): # Thresholding basically tries to look at an image and binarise the image.
                                                 # with threshold=125 and maximum=255 --> pixels that are <125 := 0 and pixels that are >125 := 255
 
-    ret, thresh = cv.threshold(img, threshold, maximum)
+    ret, thresh = cv.threshold(img, threshold, maximum, type=cv.THRESH_BINARY)
 
     return (ret, thresh)
