ab328bea92a42f90daab037ecfb754e480ba9e0f
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-02 11:04:17 +0330

    Adding more comments on how does positive and negative values affect the image.

--- _05_transformations/translation.py
+++ _05_transformations/translation.py
@@ -4,6 +4,12 @@ import numpy as np
 
 def translate(image, x, y): # x and y are number of pixels you want to shift along the x-axis and y-axis
 
+        # +x --> Right
+        # +y --> Down
+        # -x --> Left
+        # -y --> Up
+
+
     translation_matrix = np.float32([[1, 0, x], [0, 1, y]])
 
     dimensions = (image.shape[1], image.shape[0]) # image.shape[1] = width of the image and image.shape[0] = height of the image
