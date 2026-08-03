1ced795fb1e57f4776fa6d87b02557ff78b36db8
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 08:24:18 +0330

    Drawing a rectangle and a circle and displaying them.

---
+++ _10_bitwise_operations/main.py
@@ -0,0 +1,22 @@
+# There are four bitwise operators: AND, OR, XOR, NOT
+# These operators are used alot in image processing (specially with masks)
+# They operator in a binary matter, a pixel i turned off with the value 0, and it is turned on with the value 1
+
+import cv2 as cv
+import numpy as np
+
+def main():
+
+    blank = np.zeros((400, 400), dtype="uint8")
+
+    rectangle = cv.rectangle(blank.copy(), (30, 30), (370,370), 255, -1) # Since the image is represented in binary we don't have to give the color in BGR format and we can ust give one value (255 here)
+
+    circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+
+    cv.imshow("Rectangle", rectangle)
+    cv.imshow("Circle", circle)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
