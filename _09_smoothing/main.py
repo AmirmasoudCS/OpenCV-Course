512c4c9c5cd5faaf404bba3b6f6f96e0f6b72b6e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 08:05:32 +0330

    Implementing the median blur.

--- _09_smoothing/main.py
+++ _09_smoothing/main.py
@@ -24,8 +24,17 @@ def main():
     gaussian = cv.GaussianBlur(image, (3, 3), 0) # 0 is the standard deviation in the x-axis
     cv.imshow("Gaussian Blur", gaussian) 
 
+    # Median
+    # In this method, the middle pixel is the median of surrounding pixels
+    # This method tends to reduce the noise more in an image comparing to previous methods
+    # This method is mostly used in the advanced computer vision projects
+
+    median = cv.medianBlur(image, 3) # The kernel size here is only an :int, but the OpenCV assumes itself that it is a tuple of [int, int]
+    cv.imshow("Median Blur", median)
+
     cv.waitKey(0)
     cv.destroyAllWindows()
+    
 
 if __name__ == "__main__":
     main()
