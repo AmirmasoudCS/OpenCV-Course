b5ebc0a21673da8c272175b7e5404f6ed87fa4d8
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 19:42:10 +0330

    Adding some initial comments on face detection vs recognition

---
+++ _15_face_detection/main.py
@@ -0,0 +1,3 @@
+# Detection a face in an image is totally a different matter than recognizing a face in an image which in Detection, all you
+# need to do is to decide an image contains a face, or it does not contain a face (classification task)
+# while in Recognition not only you have to decide if an image contains a face or not, you also have to recognize whos face that is
