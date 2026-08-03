aedbe684ae8bf255113ffe7dfb7f22732378ed8c
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 20:52:44 +0330

    Initial commit for the saver file for face detection phase.

--- _15_face_detection/pipeline.py
+++ _15_face_detection/pipeline.py
@@ -4,6 +4,7 @@ from _15_face_detection.utils.greyscale import to_grey
 from _15_face_detection.utils.xml_loader import load_xml
 from _15_face_detection.utils.print_cords import print_cords
 from _15_face_detection.config.paths import SOURCE
+from _15_face_detection.utils.saver import save_all
 from _15_face_detection.config.constants import (
     SCALE_FACTOR,
     MINIMUM_NEIGHBOURS
@@ -12,27 +13,32 @@ from pathlib import Path
 
 def run(source_image_path: Path | None, all: bool = False):
 
-    source_image = cv.imread(str(source_image_path))
+    if not all:
 
-    if source_image is None:
-        raise FileNotFoundError(f"Could not load image: {source_image}")
+        source_image = cv.imread(str(source_image_path))
+
+        if source_image is None:
+            raise FileNotFoundError(f"Could not load image: {source_image}")
 
 
 
-    grey_image = to_grey(source=source_image)
+        grey_image = to_grey(source=source_image)
 
-    classifier = load_xml(SOURCE / "haar_face.xml")
+        classifier = load_xml(SOURCE / "haar_face.xml")
 
-    faces_rectangle = classifier.detectMultiScale(grey_image, scaleFactor=SCALE_FACTOR, minNeighbors=MINIMUM_NEIGHBOURS)
+        faces_rectangle = classifier.detectMultiScale(grey_image, scaleFactor=SCALE_FACTOR, minNeighbors=MINIMUM_NEIGHBOURS)
 
-    number_of_faces, drawn_image = print_cords(source_image, faces_rectangle)
+        number_of_faces, drawn_image = print_cords(source_image, faces_rectangle)
 
-    if number_of_faces == 0:
-        print("No faces were detected.")
+        if number_of_faces == 0:
+            print("No faces were detected.")
+        else:
+            print("Number of faces detected in the source image:", number_of_faces)
+        
+
+        cv.imshow("Detected Face", drawn_image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
     else:
-        print("Number of faces detected in the source image:", number_of_faces)
-    
-
-    cv.imshow("Detected Face", drawn_image)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+        save_all()
