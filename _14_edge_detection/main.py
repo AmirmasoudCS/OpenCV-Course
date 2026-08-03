d0a8ca6418625b5fbf274f0bf7e92ca5dccb460a
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 18:34:26 +0330

    Implementing and displaying the laplacian image,

--- _14_edge_detection/main.py
+++ _14_edge_detection/main.py
@@ -1,9 +1,23 @@
 import cv2 as cv
+import numpy as np
+
 
 def main():
 
     image = cv.imread("assets/Photos/img3.jpg")
+
+    grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+
+    # Laplacian
     
+    lap = cv.Laplacian(grey_image, cv.CV_64F)
+    lap = np.uint8(np.absolute(lap))
+
+    cv.imshow("Laplacian Image", lap)
+
+
+
 
 
     cv.waitKey(0)
