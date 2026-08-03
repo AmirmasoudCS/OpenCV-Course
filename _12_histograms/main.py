0c2503b003fc6bbf1a296efc5c5fdf33ba67bde4
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 13:18:49 +0330

    Adding the option to calculate the histogram of coloured images on a mask.

--- _12_histograms/main.py
+++ _12_histograms/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def main():
 
-    choice = input("(1) Greyscaled Histogram.\n(2) Masked Greyscaled Histogram.\n(3) Coloured Histogram.\n")
+    choice = input("(1) Greyscaled Histogram.\n(2) Masked Greyscaled Histogram.\n(3) Coloured Histogram.\n(4) Coloured Masked Histogram\n")
 
     image = cv.imread("assets/Photos/boston_park.jpg")
     grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -69,8 +69,29 @@ def main():
             plt.ylim([0, 4000])
 
         plt.show()
-        
 
+    elif choice == "4":
+
+        mask = np.zeros(image.shape[:2], dtype="uint8")
+
+        masked_image = cv.bitwise_and(image, image, mask=mask)
+
+        plt.figure()
+        plt.title("Coloured Histogram")
+        plt.xlabel("Bins")
+        plt.ylabel("# of Pixels")
+
+        colours = ('b', 'g', 'r')
+
+        for i, col in enumerate(colours):
+            hist = cv.calcHist([image], [i], mask, [256], [0, 256])
+            plt.figure(hist)
+            plt.xlim([0, 256])
+            plt.ylim([0, 4000])
+
+        plt.show()
+
+        
     else:
         print("Wrong input!")
         return
