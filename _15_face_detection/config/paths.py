caf0b2680ce895abb783f7e06e82a21d9b423101
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 20:02:59 +0330

    Adding paths of the project to the config file.

---
+++ _15_face_detection/config/paths.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+ROOT = Path(__file__).resolve().parent.parent
+
+
+UTILS = ROOT / "utils"
+ASSETS = ROOT / "assets"
+
+IMAGES = ASSETS / "images"
+SOURCE = ASSETS / "source"
