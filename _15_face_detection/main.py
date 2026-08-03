cd8844c517c0d5ddcabd420c5830ebdf016aefba
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 20:35:27 +0330

    Implementing the code for the main function of face detection.

--- _15_face_detection/main.py
+++ _15_face_detection/main.py
@@ -1,3 +1,25 @@
 # Detection a face in an image is totally a different matter than recognizing a face in an image which in Detection, all you
 # need to do is to decide an image contains a face, or it does not contain a face (classification task)
 # while in Recognition not only you have to decide if an image contains a face or not, you also have to recognize whos face that is
+from _15_face_detection.pipeline import run
+from _15_face_detection.config.paths import IMAGES
+from _15_face_detection.config.registery import IMAGES_MAPPING
+
+def get_request():
+
+    images = [f for f in IMAGES.iterdir() if f.is_file()]
+
+    print("What file do you want to perform Face Detection on?")
+    for i, image in enumerate(images, start=1):
+        print(f"{i}. {image.name}")
+
+    choice = int(input())
+
+    selected_image = images[choice - 1]
+
+    print(f"You have selected: {selected_image}")
+
+    return selected_image
+
+if __name__ == "__main__":
+    run(source_image=IMAGES_MAPPING[get_request()])
