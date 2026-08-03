bdf0fff17ce36a56c5e14e235f724f149f328624
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 18:29:00 +0330

    Reading and displaying the original image.

---
+++ _14_edge_detection/main.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+def main():
+
+    image = cv.imread("assets/Photos/img3.jpg")
+    
+
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
