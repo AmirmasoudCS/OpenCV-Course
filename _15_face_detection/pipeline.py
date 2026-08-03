9a9bffb70a102c5ed10a13bad5758ad289253e21
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-03 20:19:27 +0330

    Printing out the number of faces detected and the face detected is shown.

--- _15_face_detection/pipeline.py
+++ _15_face_detection/pipeline.py
@@ -1,5 +1,8 @@
+import cv2 as cv
+
 from _15_face_detection.utils.greyscale import to_grey
 from _15_face_detection.utils.xml_loader import load_xml
+from _15_face_detection.utils.print_cords import print_cords
 from _15_face_detection.config.paths import SOURCE
 from _15_face_detection.config.constants import (
     SCALE_FACTOR,
@@ -15,4 +18,7 @@ def run(source_image):
 
     faces_rectangle = classifier.detectMultiScale(grey_image, scaleFactor=SCALE_FACTOR, minNeighbors=MINIMUM_NEIGHBOURS)
 
-    print(f"Number of faces found: {len(faces_rectangle)}.")
+    number_of_faces, drawn_image = print_cords(source_image, faces_rectangle)
+
+    print("Number of faces detected in the source image:", number_of_faces)
+    cv.imshow("Detected Face", drawn_image)
