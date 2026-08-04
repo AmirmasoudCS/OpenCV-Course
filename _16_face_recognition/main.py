60f67d681c5739d59a72172942890290b60f84ed
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-04 18:07:57 +0330

    Training the face recognizer.

--- _16_face_recognition/main.py
+++ _16_face_recognition/main.py
@@ -1,6 +1,8 @@
 from _16_face_recognition.train import create_train
 from _16_face_recognition.saver import save
 from _16_face_recognition.loader import load_train
+import cv2 as cv
+import numpy as np
 
 def main():
     overwrite: bool = int(input("Overwrite the train results?\n(0) No    (1) Yes\n"))
@@ -12,7 +14,20 @@ def main():
     else:
         features, labels = load_train()
 
-    print(len(features), labels)
+    features = np.array(features, dtype='object')
+    labels = np.array(labels)
+
+
+    face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+    # Train the Recognizer on the features list and the labels list
+
+    face_recognizer.train(features, labels)
+
+    np.save("config/features.npy", features)
+    np.save("config/labels.npy", labels)
+
+
         
 
 if __name__ == "__main__":
