4bae3ba354fe87ccef3873734394546fc6fea0a7
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-04 17:13:02 +0330

    Updating the path constatns.

--- _16_face_recognition/config/paths.py
+++ _16_face_recognition/config/paths.py
@@ -4,8 +4,15 @@ ROOT = Path(__file__).resolve().parent.parent
 
 ASSETS = ROOT / "assets"
 IMAGES = ASSETS / "images"
+TRAIN = IMAGES / "train"
+VALIDATE = IMAGES / "validate"
 
-GEORGE_HARRISON = IMAGES / "George_Harrison"
-JOHN_LENNON = IMAGES / "Jhon_Lennon"
-PAUL_MCCARTENY = IMAGES / "Paul_McCarteny"
-RINGO_STARR = IMAGES / "Ringo_Starr"
+GEORGE_HARRISON_TRAIN = TRAIN / "George_Harrison"
+JOHN_LENNON_TRAIN = TRAIN / "Jhon_Lennon"
+PAUL_MCCARTENY_TRAIN = TRAIN / "Paul_McCarteny"
+RINGO_STARR_TRAIN = TRAIN / "Ringo_Starr"
+
+GEORGE_HARRISON_VALIDATE = VALIDATE / "George_Harrison"
+JOHN_LENNON_VALIDATE = VALIDATE / "Jhon_Lennon"
+PAUL_MCCARTENY_VALIDATE = VALIDATE / "Paul_McCarteny"
+RINGO_STARR_VALIDATE = VALIDATE / "Ringo_Starr"
