b7345c9632606876dc15ae71c6f0e76fb0b0f385
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-04 17:02:14 +0330

    Implementing soem paths of the directory.

---
+++ _16_face_recognition/config/paths.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+
+ASSETS = ROOT / "assets"
+IMAGES = ASSETS / "images"
+
+GEORGE_HARRISON = IMAGES / "George_Harrison"
+JOHN_LENNON = IMAGES / "Jhon_Lennon"
+PAUL_MCCARTENY = IMAGES / "Paul_McCarteny"
+RINGO_STARR = IMAGES / "Ringo_Starr"
